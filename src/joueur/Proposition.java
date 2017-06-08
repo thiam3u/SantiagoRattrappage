a2cfbb9960c6810d9940e237aa53e5454cdbc9d0
nadir-youcef <nadir.boumaraf@gmail.com>
2017-06-08 12:32:29 +0200

    import + attributs classe Proposition

--- src/joueur/Proposition.java
+++ src/joueur/Proposition.java
@@ -1,7 +1,12 @@
 package joueur;
 
-
+import java.util.ArrayList;
+import plateau.Canal;
 
 public class Proposition {
+	
+    ArrayList<Joueur> listJoueur = new ArrayList<Joueur>();
+    ArrayList<Integer> listMontant = new ArrayList<Integer>();
+    Canal canal;
 
 }
