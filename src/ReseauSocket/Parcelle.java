b5eeeea9b46369e802b79f9ba9ec13b4faee9d24
thiam3u <thiamm053@gmail.com>
2017-06-14 14:33:59 +0200

    correcction quleques de mes commits précédent et qlq  l'amélioration  de la calsse client

---
+++ src/ReseauSocket/Parcelle.java
@@ -0,0 +1,5 @@
+package ReseauSocket;
+
+public class Parcelle {
+
+}
--- src/ReseauSocket/Serveur.java
+++ src/ReseauSocket/Serveur.java
@@ -10,6 +10,8 @@ import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 
+
+
 //import reseau.Server;
 //import reseau.Server2Connection;
 
@@ -21,7 +23,7 @@ public class Serveur {
 	    Socket clientSocket = null;
 	    int numConnections = 0;
 	    int port;
-	   // ArrayList<Server2Connection> clientCo = new ArrayList<Server2Connection>();
+	    ArrayList<Server2Connection> clientCo = new ArrayList<Server2Connection>();
 
 
 	    public Serveur(int port) {
@@ -32,6 +34,7 @@ public class Serveur {
 	        System.out.println("Server cleaning up.");
 	        System.exit(0);
 	    }
+	    
 
 	    public void startServer() {
 	        
@@ -44,9 +47,9 @@ public class Serveur {
 	                        try {
 	                            clientSocket = echoServer.accept();
 	                            numConnections++;
-	                            //Server2Connection oneconnection = new Server2Connection(clientSocket, numConnections, Server.this);
-	                           // clientCo.add(oneconnection);
-	                           // new Thread(oneconnection).start();
+	                            Server2Connection oneconnection = new Server2Connection(clientSocket, numConnections, Serveur.this);
+	                            clientCo.add(oneconnection);
+	                            new Thread(oneconnection).start();
 	                        } catch (IOException e) {
 	                            System.out.println(e);
 	                        }
@@ -56,5 +59,134 @@ public class Serveur {
 	                }
 	            }
 	        };
+
+	    
+	    }
+	    
+	    
+	    public String getPseudo(int id) {
+	        return clientCo.get(id).pseudoJoueur;
+	    }
+
+	    public int getNbCo() {
+	        return numConnections;
+	    }
+
+	    public void sendJoueur(ArrayList<Joueur> listeJoueurs, int i) {
+	        System.out.println("sendJoueur");
+	        try {
+	            clientCo.get(i).oos.reset();
+	            clientCo.get(i).oos.writeInt(2);
+	            clientCo.get(i).oos.flush();
+	            for(int j =0; j<4; j++){
+	                clientCo.get(i).oos.writeObject(listeJoueurs.get(j));
+	                clientCo.get(i).oos.flush();
+
+	            }
+	            //clientCo.get(i).oos.reset();
+	            //Thread.sleep(800);
+	        } catch (IOException e) {
+	            System.out.println("fail envoie");
+	            e.printStackTrace();
+	        }
+	    }
+	    
+	    
+	    
+	    class Server2Connection implements Runnable {
+	        ObjectInputStream is;
+	        //PrintStream os;
+	        ObjectOutputStream oos = null;
+	        Socket clientSocket;
+	        int id;
+	        Serveur server;
+	        Boolean recu = false;
+	        String pseudoJoueur = "unknow";
+	        int montantEnchereInt = -1;
+	        Thread att;
+	         public Parcelle parcelleMain;
+
+	        public Server2Connection(Socket clientSocket, int id, Serveur server) {
+	            this.clientSocket = clientSocket;
+	            this.id = id;
+	            this.server = server;
+	            System.out.println( "Connection " + id + " established with: " + clientSocket );
+	            try {
+	                is = new ObjectInputStream(clientSocket.getInputStream());
+	                //os = new PrintStream(clientSocket.getOutputStream());
+	                oos = new ObjectOutputStream(clientSocket.getOutputStream());
+	            } catch (IOException e) {
+	                System.out.println(e);
+	            }
+
+	            try {
+	                System.out.println("demande pseudo");
+	                oos.writeInt(1);
+	                oos.flush();
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+
+	        public void run() {
+	            int n;
+	            System.out.println("client thread crée");
+	            try {
+	                boolean serverStop = false;
+
+	                while (true) {
+	                    n = is.readInt();
+	                    System.out.println( "Received " + n + " from Connection " + id + "." );
+
+
+	                    //1 = pseudo
+	                    if(n == 1){
+	                        try {
+	                            pseudoJoueur = (String) is.readObject();
+	                        } catch (ClassNotFoundException e) {
+	                            e.printStackTrace();
+	                        }
+	                        System.out.println(pseudoJoueur);
+	                    }
+	                    if(n == 2){
+	                        recu = true;
+	                    }
+	                    if(n == 3){
+	                        montantEnchereInt = is.readInt();
+	                        synchronized (att){
+	                            att.notify();
+	                        }
+	                    }
+	                    if(n == 4){
+	                        try {
+	                            parcelleMain = (Parcelle) is.readObject();
+	                            synchronized (att){
+	                                att.notify();
+	                            }
+	                        } catch (ClassNotFoundException e) {
+	                            e.printStackTrace();
+	                        }
+	                    }
+	                    if(serverStop){
+	                        break;
+	                    }
+
+	                }
+
+	                System.out.println( "Connection " + id + " closed." );
+	                is.close();
+	                //os.close();
+	                oos.close();
+	                clientSocket.close();
+
+	                if ( serverStop ) server.stopServer();
+	            } catch (IOException e) {
+	                System.out.println(e);
+	            }
+	        }  
+
 }
+	    
+	    
+	    
 }
